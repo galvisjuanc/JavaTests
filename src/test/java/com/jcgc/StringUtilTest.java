d6bbc7dabafbd706c5bca295dfdce7c272902648
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 12:36:04 -0500

    Refactoring code using JUnit annotation

--- src/test/java/com/jcgc/StringUtilTest.java
+++ src/test/java/com/jcgc/StringUtilTest.java
@@ -1,12 +1,14 @@
 package com.jcgc;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringUtilTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testRepeat() {
         Assertions.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
         Assertions.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
