98254f59ec4951f21448209de00829b1f8f6d8a6
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 15:15:27 -0500

    Use Mockito in specific tests

--- src/test/java/com/jcgc/PlayerTest.java
+++ src/test/java/com/jcgc/PlayerTest.java
@@ -1,6 +1,7 @@
 package com.jcgc;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +9,20 @@ class PlayerTest {
 
     @Test
     public void lose_when_dice_number_is_too_low() {
-        Dice dice = new Dice(6);
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+
         Player player = new Player(dice, 3);
-        assertEquals(false, player.play());
+        assertFalse(player.play());
+    }
+
+    @Test
+    public void wins_when_dice_number_is_big() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
     }
 
 }
