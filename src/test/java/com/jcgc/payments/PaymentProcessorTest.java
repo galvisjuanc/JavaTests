a8b9095019fd3a3d9109f27597e6b9f15d2a3074
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 16:17:21 -0500

    Add new test and variables before the tests

--- src/test/java/com/jcgc/payments/PaymentProcessorTest.java
+++ src/test/java/com/jcgc/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.jcgc.payments;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -7,28 +8,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentProcessorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @BeforeEach
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct(){
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertTrue(paymentProcessor.makePayment(1000));
-
     }
 
     @Test
     public void payment_is_wrong(){
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
-
     }
 
 }
