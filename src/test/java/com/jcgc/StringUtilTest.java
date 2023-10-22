f25e16792b8bc4079410013d7c670b67a46dba57
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 12:23:03 -0500

    JUnit Test

--- src/test/java/com/jcgc/StringUtilTest.java
+++ src/test/java/com/jcgc/StringUtilTest.java
@@ -1,19 +1,14 @@
 package com.jcgc;
 
-//import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringUtilTest {
+
     public static void main(String[] args) {
-
-        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
-        assertEquals(StringUtil.repeat("hola", 1), "hola");
-
-    }
-
-    private static void assertEquals(String actual, String expected) {
-        if(!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equals to expected value --> " + expected);
-        }
+        Assertions.assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+        Assertions.assertEquals(StringUtil.repeat("hola", 1), "hola");
     }
 
 }
