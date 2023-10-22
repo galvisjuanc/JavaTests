f81b4c520fa1ce2568473bf219b4ff11efcdb853
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 12:47:14 -0500

    Manage test with JUnit for every possible case

--- src/test/java/com/jcgc/StringUtilTest.java
+++ src/test/java/com/jcgc/StringUtilTest.java
@@ -8,9 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-        Assertions.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    public void repeat_string_once() {
         Assertions.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
 
+    @Test
+    public void repeat_string_multiple_times() {
+        Assertions.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assertions.assertEquals("", StringUtil.repeat("hola", 0));
+    }
 }
