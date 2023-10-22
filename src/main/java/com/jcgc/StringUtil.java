56a60fed3bd7ab566a6c31f43a2b1d2190f11df1
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 12:54:01 -0500

    New test added to verify exceptions

--- src/main/java/com/jcgc/StringUtil.java
+++ src/main/java/com/jcgc/StringUtil.java
@@ -5,6 +5,9 @@ import java.util.stream.IntStream;
 public class StringUtil {
     public static String repeat(String str, int times) {
 
+        if (times < 0)
+            throw new IllegalArgumentException("negative values are not allowed");
+
         String result = "";
 
         for (int i = 0; i < times; i++) {
