853292eae1d9750095e0e55ea72a9e97460049ab
Juan Galvis <galvisjuanc@gmail.com>
2023-10-22 14:37:27 -0500

    New tests to validate a password using JUnit

--- src/test/java/com/jcgc/PasswordUtilTest.java
+++ src/test/java/com/jcgc/PasswordUtilTest.java
@@ -2,6 +2,7 @@ package com.jcgc;
 
 import org.junit.jupiter.api.Test;
 
+import static com.jcgc.PasswordUtil.SecurityLevel.MEDIUM;
 import static com.jcgc.PasswordUtil.SecurityLevel.WEAK;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +11,15 @@ class PasswordUtilTest {
     @Test
     void weak_when_has_less_than_8_letters() {
         assertEquals(WEAK, PasswordUtil.assessPassword("1234567"));
+    }
+
+    @Test
+    void weak_when_has_only_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abcdefgh"));
+    }
+
+    @Test
+    void medium_when_has_letters_and_numbers() {
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcd1234"));
     }
 }
