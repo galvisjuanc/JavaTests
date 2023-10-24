6435f2e829cb87e89f73bbc864f05e1706dfc394
Juan Galvis <galvisjuanc@gmail.com>
2023-10-23 20:41:56 -0500

    Add logic in DateUtil

--- src/main/java/com/jcgc/util/DateUtil.java
+++ src/main/java/com/jcgc/util/DateUtil.java
@@ -3,6 +3,10 @@ package com.jcgc.util;
 public class DateUtil {
 
     public static boolean isLeapYear(int year) {
+
+        if (year % 4 == 0 && year % 100 != 0){
+            return true;
+        }
         if (year % 400 == 0){
             return true;
         } else {
