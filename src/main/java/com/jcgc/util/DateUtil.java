9c8929a968a383fbc68884a8dfd074659f50067c
Juan Galvis <galvisjuanc@gmail.com>
2023-10-23 20:42:56 -0500

    Refactor code

--- src/main/java/com/jcgc/util/DateUtil.java
+++ src/main/java/com/jcgc/util/DateUtil.java
@@ -4,10 +4,7 @@ public class DateUtil {
 
     public static boolean isLeapYear(int year) {
 
-        if (year % 4 == 0 && year % 100 != 0){
-            return true;
-        }
-        if (year % 400 == 0){
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
             return true;
         } else {
             return false;
