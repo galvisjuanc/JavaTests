33db9da4508a62bbd7675541ad6ae67a31ceffc8
Juan Galvis <galvisjuanc@gmail.com>
2023-10-23 20:37:36 -0500

    TDD examples

---
+++ src/main/java/com/jcgc/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.jcgc.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
