180ac8bdb39c30c9a14dd68b26f5c4243fbc9f51
Juan Galvis <galvisjuanc@gmail.com>
2023-10-25 20:05:21 -0500

    Tests for movies

--- src/main/java/com/jcgc/movies/service/MovieService.java
+++ src/main/java/com/jcgc/movies/service/MovieService.java
@@ -1,4 +1,23 @@
 package com.jcgc.movies.service;
 
+import com.jcgc.movies.data.MovieRepository;
+import com.jcgc.movies.model.Genre;
+import com.jcgc.movies.model.Movie;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 public class MovieService {
+
+    private MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Collection<Movie> findMoviesByGenre(Genre genre) {
+
+        return movieRepository.findAll().stream()
+                .filter(movie -> movie.getGenre() == genre).collect(Collectors.toList());
+    }
 }
